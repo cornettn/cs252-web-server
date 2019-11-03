75324028f0722bafe1552b6586809a7d09ad63ef
Noah Liam Cornett <cornettn@data.cs.purdue.edu>
2019-11-03 17:04:18 -0500

    Latest build

--- defaults.c
+++ defaults.c
@@ -8,6 +8,6 @@ http_response handle_default(const http_request *request) {
     http_response resp = { 0 };
     resp.http_version = request->http_version;
     resp.status_code = 404;
-    resp.reason_phrase = status_reason(resp.status_code);
+    resp.reason_phrase = status_reason(404);
     return resp;
 } /* handle_default() */
