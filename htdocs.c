074656a6a4c85cbc78126a79251182ef5fd8683b
Noah Liam Cornett <cornettn@data.cs.purdue.edu>
2019-11-03 10:48:03 -0500

    Latest build

--- htdocs.c
+++ htdocs.c
@@ -1,20 +1,25 @@
 #include "http_messages.h"
 
 #include <stdio.h>
+#include <unistd.h>
 
-#define REFERER ("Referer")
 /*
  * You should implement this function and use it in server.c
  */
 
-http_response handle_htdocs(const http_request *request) {
-    http_response resp = { 0 };
-    resp.http_version = request->http_version;
+http_response *handle_htdocs(const http_request *request) {
+    http_response *resp = (http_response *) malloc(sizeof(http_reponse));
+    resp->http_version = request->http_version;
+    resp->num_headers = 0;
+    resp->headers = NULL;
 
     // char *referer = get_header_value(request, REFERER);
     // printf("%s\n", referer);
 
     // TODO: Get the request URL, verify the file exists, and serve it
+
+    char *URL = request->request_uri;
+
 
     return resp;
 }
