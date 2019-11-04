ee0a9938dbd52d3338392dce1f5db5469116622c
Noah Liam Cornett <cornettn@data.cs.purdue.edu>
2019-11-04 12:30:54 -0500

    Latest build

--- routes.h
+++ routes.h
@@ -6,8 +6,6 @@
 http_response handle_cgi_bin(const http_request *request);
 http_response handle_htdocs(const http_request *request);
 http_response handle_default(const http_request *request);
-http_response handle_forbidden(const http_request *request);
-http_response handle_request(const http_request *request, int status_code);
 
 typedef http_response (*route_handle) (const http_request *);
 
@@ -15,13 +13,12 @@ typedef struct{
     char *route_name;
     route_handle handle_func;
 }  routes;
-/*
+
 // A routes map, pairing string served and handle function
 routes route_map[] = {
     {"/cgi-bin", &handle_cgi_bin},
     {"/", &handle_htdocs},
     {"", &handle_default}
 };
-*/
 
 #endif  // ROUTES_H
