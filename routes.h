2e078638a7bafc30b7ca368ca3995b9a257c1697
Noah Liam Cornett <cornettn@data.cs.purdue.edu>
2019-11-13 15:48:34 -0500

    Latest build

--- routes.h
+++ routes.h
@@ -14,11 +14,5 @@ typedef struct{
     route_handle handle_func;
 }  routes;
 
-// A routes map, pairing string served and handle function
-routes route_map[] = {
-    {"/cgi-bin", &handle_cgi_bin},
-    {"/", &handle_htdocs},
-    {"", &handle_default}
-};
 
 #endif  // ROUTES_H
