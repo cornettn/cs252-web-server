2cdbde37056e66d222c5e81734b833e46de4162d
Noah Liam Cornett <cornettn@data.cs.purdue.edu>
2019-11-13 23:04:35 -0500

    Latest build

--- http_messages.c
+++ http_messages.c
@@ -120,6 +120,8 @@ char *append(char *str, char *appen, int len1, int len2) {
   char *new_str = (char *) malloc(len1 + len2);
   memcpy(new_str, str, len1);
   memcpy(new_str + len1, appen, len2);
+  free(str);
+  str = NULL;
   new_str[len1 + len2] = '\0';
   return new_str;
 } /* append() */
